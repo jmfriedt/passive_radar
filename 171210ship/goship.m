b0325f99801f16a0b53c03312697eaac5cd7f517
Jean-Michel Friedt <friedtj@free.fr>
2026-02-14 09:08:16 +0100

    remove unnecessary error at end of execution

--- 171210ship/goship.m
+++ 171210ship/goship.m
@@ -76,63 +76,59 @@ for k=1:N:filesize/datasize/2 % 19227738112/4
   t=fread(f2,N,datatype);
   mes=t(1:2:end)+j*t(2:2:end);
 
-  if (pos>0)              % align
-     mes=mes(pos:end);
-     ref=ref(1:end-pos+1);
-  else
-     ref=ref(-pos+1:end);
-     mes=mes(1:end+pos);
-  end
-  if (p==1)
-    figure
-    plot(([-length(ref)+1:length(ref)-1])*3E8/fs/1000,abs(xcorr(ref,mes)))
-    xlim([-20 20]);xlabel('range (km)');ylabel('xcorr (a.u.)');text(10,6e8,'future');text(-10,6e8,'past');
-    figure
-  end
+  if length(mes)==N/2
+    if (pos>0)              % align
+       mes=mes(pos:end);
+       ref=ref(1:end-pos+1);
+    else
+       ref=ref(-pos+1:end);
+       mes=mes(1:end+pos);
+    end
+    if (p==1)
+      figure
+      plot(([-length(ref)+1:length(ref)-1])*3E8/fs/1000,abs(xcorr(ref,mes)))
+      xlim([-20 20]);xlabel('range (km)');ylabel('xcorr (a.u.)');text(10,6e8,'future');text(-10,6e8,'past');
+      figure
+    end
 
-  if (dsi_suppression==1)
-      nt=length(ref);
-      %% DSI suppression -- algorithm implementation provided by W. Feng (X'ian, China)
-      % Range shift
-      Index1=-9;Index2=+9;  % negative range if doubts about ref v.s meas
-      num_range_shift=(Index2-Index1+1);
-      X1=zeros(nt,num_range_shift);
-      for kk=Index1:Index2
-          te=kk+abs(Index1)+1;
-          if kk<=0
-              X1(:,te)=[ref(0-kk+1:end);zeros(0-kk,1)];
-          else
-              X1(:,te)=[zeros(kk-1,1);ref(1:end-kk+1)];
-          end
-      end
-      mes=mes-X1*(pinv(X1)*mes); % Least Square optimization
-      clear X1;
-  end
+    if (dsi_suppression==1)
+        nt=length(ref);
+        %% DSI suppression -- algorithm implementation provided by W. Feng (X'ian, China)
+        % Range shift
+        Index1=-9;Index2=+9;  % negative range if doubts about ref v.s meas
+        num_range_shift=(Index2-Index1+1);
+        X1=zeros(nt,num_range_shift);
+        for kk=Index1:Index2
+            te=kk+abs(Index1)+1;
+            if kk<=0
+                X1(:,te)=[ref(0-kk+1:end);zeros(0-kk,1)];
+            else
+                X1(:,te)=[zeros(kk-1,1);ref(1:end-kk+1)];
+            end
+        end
+        mes=mes-X1*(pinv(X1)*mes); % Least Square optimization
+        clear X1;
+    end
 
-  m=1;
-  for fd=freq
-     mesdop=mes.*exp(j*2*pi*fd*tim);
-     x=abs(xcorr(ref,mesdop,dN));
-     rangedop(:,m)=x(dN-15:dN+150);
-     m=m+1;
-  end          % vvv flipud => reverse Y axis wrt rangedop
-  imagesc(freq,([-150:+15]+1)*3e8/fs/2/1000,fliplr(flipud(rangedop)),[0 scf*max(max(rangedop))]);
-  xlabel('Doppler shift (Hz)')
-  ylabel('range (km)')
-  temps=p/2;
-  colorbar
-  title([num2str(temps),' s'])     
-  if (save_picts==1)
-    name=[num2str(p,'%04d'),'_2.png'];
-    eval(['print -dpng ',name]);
-    name=[num2str(p,'%04d'),'_2.mat'];
-    eval(['save ',name,' rangedop']);
+    m=1;
+    for fd=freq
+       mesdop=mes.*exp(j*2*pi*fd*tim);
+       x=abs(xcorr(ref,mesdop,dN));
+       rangedop(:,m)=x(dN-15:dN+150);
+       m=m+1;
+    end          % vvv flipud => reverse Y axis wrt rangedop
+    imagesc(freq,([-150:+15]+1)*3e8/fs/2/1000,fliplr(flipud(rangedop)),[0 scf*max(max(rangedop))]);
+    xlabel('Doppler shift (Hz)')
+    ylabel('range (km)')
+    temps=p/2;
+    colorbar
+    title([num2str(temps),' s'])
+    if (save_picts==1)
+      name=[num2str(p,'%04d'),'_2.png'];
+      eval(['print -dpng ',name]);
+      name=[num2str(p,'%04d'),'_2.mat'];
+      eval(['save ',name,' rangedop']);
+    end
+    p=p+1;
   end
-  p=p+1;
-end
-% r=linspace(0,67/2.048*300/2,67)
-% xx=x(2048+160:2048+226,:);
-if ~(exist('simulated'))
-  r=linspace(0,157/2.048*300/2,157)
-  xx=x(2048+450:2048+606,:);
 end
