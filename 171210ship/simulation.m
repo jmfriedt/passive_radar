7122f02e2aa8dd970689e454f5448a1be783f78c
Jean-Michel Friedt <friedtj@free.fr>
2026-02-15 08:31:53 +0100

    correct simulation resolution

--- 171210ship/simulation.m
+++ 171210ship/simulation.m
@@ -1,14 +1,14 @@
-fs=2e6;
-datatype='int16';          % can be int8, int16, int32 or float
+fs=2.048e6;
+datatype='int8';          % can be int8, int16, int32 or float
 eval(["tmp=",datatype,"(3);"]);
-datasize=sizeof(tmp);
+datasize=sizeof(tmp)
 ref=randn(4*fs,1);
 tim=[0:length(ref)-1]'/fs;
 lo=exp(j*2*pi*50*tim);
 sur=ref+[ref(101:end) ; ref(1:100)].*lo;
 if (strcmp(datatype,'float')==0)  % round to integer values
-  sur=round(sur/max(abs(sur))*(2^datasize-1));
-  ref=round(ref/max(abs(ref))*(2^datasize-1));
+  sur=round(sur/max(abs(sur))*(2^(8*datasize-1)));
+  ref=round(ref/max(abs(ref))*(2^(8*datasize-1)));
 end
 f=fopen('simulation_ref.bin','wb');fwrite(f,ref,datatype);fclose(f);
 f=fopen('simulation_sur.bin','wb');fwrite(f,sur,datatype);fclose(f);
